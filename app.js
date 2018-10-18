64ace9ad2b242f38fc0c0394b71da48e1a701b80
Jonathan Peters <42306733+JonathanPetersCode@users.noreply.github.com>
2018-10-18 19:52:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 60;
+let val;
+
+// Simple Math with Numbers
+
+val = num1 + num2;
+val = num1 / num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 % num2;
+
+//Math
+
+val = Math.PI;
+val = Math.E;
+val = Math.floor(2.3);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(10, 23, 59, -2);
+val = Math.max(10, 23, 59, 1);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);
+
+
+console.log(val);
